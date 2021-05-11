3f0670175839d0b86da5753734c378b5f1e5ca18
Fira <mekkiti@gmail.com>
2021-05-11 15:33:33 +0000

    refinements

--- updatetag.sh
+++ updatetag.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Just pull a diff source to be used by the production setup for next restart
 # If unspecified, updates the tag with current set image source
-IMAGE_SOURCE=${1:$(cat image-source)}
-docker pull "$1" && echo "$1" > $(dirname "$0")/image-source && docker tag "$1" cm13-live
+BASEDIR=`dirname "$0"`
+CURRENT_IMAGE_SOURCE=`cat ${BASEDIR}/image-source`
+IMAGE_SOURCE="${1:-$CURRENT_IMAGE_SOURCE}"
+docker pull "${IMAGE_SOURCE}"
+echo "${IMAGE_SOURCE}" > ${BASEDIR}/image-source
+docker tag "${IMAGE_SOURCE}" cm13-live
