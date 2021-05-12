f53ab3455a165f0f44776deb68a6dcd52f608030
Fira <mekkiti@gmail.com>
2021-05-12 03:57:35 +0000

    dynamic testing changes

--- start-testing.sh
+++ start-testing.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Just starts testing environment for provided image
+BASEDIR=`dirname "$0"`
 TARGET_IMAGE="$1"
 ESCAPED_NAME=`systemd-escape "$1"`
-systemctl start "cm13-testing@${ESCAPED_NAME}.service" > /dev/null
-
+systemctl restart "cm13-testing@${ESCAPED_NAME}.service"
+INSTANCE_URL=`${BASEDIR}/get-test-url.sh ${TARGET_IMAGE} 2>/dev/null`
+echo $INSTANCE_URL
