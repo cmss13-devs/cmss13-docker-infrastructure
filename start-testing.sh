ee0b361a84525dd3e4ace916d73beb84c22b88f8
Fira <mekkiti@gmail.com>
2021-07-09 14:19:53 +0000

    Remove gitlab dynamic test tooling

--- start-testing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Just starts testing environment for provided image
-BASEDIR=`dirname "$0"`
-TARGET_IMAGE="$1"
-ESCAPED_NAME=`systemd-escape "$1"`
-systemctl restart "cm13-testing@${ESCAPED_NAME}.service"
-INSTANCE_URL=`${BASEDIR}/get-test-url.sh ${TARGET_IMAGE} 2>/dev/null`
-echo $INSTANCE_URL
--- stop-testing.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Just starts testing environment for provided image
-ESCAPED_NAME=`systemd-escape "$1"`
-systemctl stop "cm13-testing@${ESCAPED_NAME}.service"
